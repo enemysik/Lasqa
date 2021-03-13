2006b96b64af3c9ce1f82fc9b1e4e94ea5be5c2c
Tony <enemysik@gmail.com>
2021-03-13 19:31:07 +0700

    i

---
+++ Lasqa.Logic/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Lasqa.Logic
+{
+    public class PathCreator
+    {
+        public Path Create()
+        {
+            return new Path
+            {
+                Valid = true,
+            };
+        }
+    }
+    public class Path
+    {
+        public bool Valid { get; set; }
+    }
+}
---
+++ Lasqa.Tests/Class1.cs
@@ -0,0 +1,65 @@
+﻿using Lasqa.Logic;
+using NUnit.Framework;
+
+namespace Lasqa.Tests
+{
+    public class PathCreatorTests
+    {
+        private PathCreator _pathCreator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pathCreator = new PathCreator();
+        }
+
+        [Test]
+        public void ShouldCreatePath()
+        {
+            var path = _pathCreator.Create();
+
+            Assert.NotNull(path);
+        }
+
+        [Test]
+        public void ShouldCreateValidPath()
+        {
+            var path = _pathCreator.Create();
+
+            Assert.IsTrue(path.Valid);
+        }
+    }
+
+    public class PathCreatorTests2
+    {
+        private PathCreator _pathCreator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pathCreator = new PathCreator();
+        }
+
+        [Test]
+        public void ShouldCreatePath()
+        {
+            var path = _pathCreator.Create();
+
+            Assert.NotNull(path);
+        }
+
+        [Test]
+        public void ShouldCreateValidPath()
+        {
+            var path = _pathCreator.Create();
+
+            Assert.IsTrue(path.Valid);
+        }
+
+        [Test]
+        public void AlwaysFails()
+        {
+            Assert.Fail();
+        }
+    }
+}
